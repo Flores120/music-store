df043fad2b486697e0a0c692531064794da2441d
David Ayala <dpayala@hotmail.com>
2016-10-18 08:58:21 -0700

    Version 1.0

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'my-app',
+  template: `
+  <div class="container">
+    <h1>My first Angular2 App</h1>
+  </div>
+  `
+})
+
+export class AppComponent{
+
+}
